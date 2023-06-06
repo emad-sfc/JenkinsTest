e9d6d790a47d213df2130ccbade1e11addf81019
unknown <emad.sfc@gmail.com>
2023-06-06 22:34:10 +0530

    ifirst commit
     Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/com/example/testJenkins/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.testJenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "Hello World. This is Jenkins build.";
+    }
+
+}
